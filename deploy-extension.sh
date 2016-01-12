1f210f542e125100a0ca1514cb8018f96a60ae82
Ben Friedland <ben174@gmail.com>
2016-01-12 09:42:50 -0800

    Version bump: 1.1.3 -> 1.1.4

--- deploy-extension.sh
+++ deploy-extension.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm rick-roulette.zip
-zip rick-roulette.zip extension/*
+zip -r rick-roulette.zip extension/*
 
 
 #TODO: Figure out how to deploy to chrome developer dashboard via api
