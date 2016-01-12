3c9c7d4397a52df9c6b649cd97ae2e8356f9d941
Ben Friedland <ben174@gmail.com>
2016-01-12 12:48:26 -0800

    Deployment script cleans unnecessary files

--- deploy-extension.sh
+++ deploy-extension.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm rick-roulette.zip
-zip -r rick-roulette.zip extension/*
-
+find . -name "*.un~" -exec rm -rf {} \;
+zip -r rick-roulette.zip extension/* -x "*.DS_Store"
 
 #TODO: Figure out how to deploy to chrome developer dashboard via api
